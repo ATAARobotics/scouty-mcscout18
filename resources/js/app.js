e5dd4d9c4c2f68769b4e026738573e48f73030fc
John Brandt <johnb0@outlook.com>
2017-10-29 16:48:18 -0600

    Rename javascript file and test database syncing and update navbar

---
+++ resources/js/app.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function(){
+ $('#Submit').on('click', function(e) {
+
+ var first = $('#fname').val(); 
+ var last = $('#lname').val();
+ var round = $('#round').val();
+ var db = new PouchDB('kittens');
+ var doc = {
+  "_id": round,
+  "First": first,
+  "Last": last,
+};
+
+
+db.put(doc);
+
+
+  db.info().then(function (info) {
+  console.log(info);
+})
+ }); 
+});
+
+
+
+$(document).ready(function(){
+ $('#Sync').on('click', function(e) {
+PouchDB.sync('kittens', 'http://localhost:5984/kittens');
+ }); 
+});
--- resources/js/test.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function() {
-  $('#test').on('click', function(e) {
-  var db = new PouchDB('http://localhost:5984/kittens');
-  db.info().then(function (info) {
-  console.log(info);
-})
-  });
-});
