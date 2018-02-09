d83c747e35d62bdcb39c180caefbfa2e57dd6194
John Brandt <johnb0@outlook.com>
2018-02-08 20:29:01 -0700

    Remove camera plugin add camera plugin permission fix add robot photo

--- app/platforms/ios/Scouty-McScout18.xcarchive/Products/Applications/Scouty-McScout18.app/www/cordova_plugins.js
+++ app/platforms/ios/Scouty-McScout18.xcarchive/Products/Applications/Scouty-McScout18.app/www/cordova_plugins.js
@@ -21,7 +21,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-wkwebview-engine": "1.1.4"
+  "cordova-plugin-wkwebview-engine": "1.1.4",
+  "cordova-plugin-ios-camera-permissions": "1.2.0"
 };
 // BOTTOM OF METADATA
 });
--- app/platforms/ios/www/cordova_plugins.js
+++ app/platforms/ios/www/cordova_plugins.js
@@ -15,38 +15,6 @@ module.exports = [
     "clobbers": [
       "window.WkWebView"
     ]
-  },
-  {
-    "id": "cordova-plugin-camera.Camera",
-    "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "Camera"
-    ]
-  },
-  {
-    "id": "cordova-plugin-camera.CameraPopoverOptions",
-    "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "CameraPopoverOptions"
-    ]
-  },
-  {
-    "id": "cordova-plugin-camera.camera",
-    "file": "plugins/cordova-plugin-camera/www/Camera.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "navigator.camera"
-    ]
-  },
-  {
-    "id": "cordova-plugin-camera.CameraPopoverHandle",
-    "file": "plugins/cordova-plugin-camera/www/ios/CameraPopoverHandle.js",
-    "pluginId": "cordova-plugin-camera",
-    "clobbers": [
-      "CameraPopoverHandle"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -54,7 +22,7 @@ module.exports.metadata =
 {
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-plugin-wkwebview-engine": "1.1.4",
-  "cordova-plugin-camera": "4.0.2"
+  "cordova-plugin-ios-camera-permissions": "1.2.0"
 };
 // BOTTOM OF METADATA
 });
