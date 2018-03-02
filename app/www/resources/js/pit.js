0bfcbb667a245ee6319d87b984c3dbbc3f5504ff
John Brandt <johnb0@outlook.com>
2018-03-01 18:31:25 -0700

    Show photo that was submitted when editing

--- app/www/resources/js/pit.js
+++ app/www/resources/js/pit.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
     $('#teamNumber').on('paste keyup', function (e) {
         var teamNumber = $('#teamNumber').val();
         var id = "pit_" + teamNumber;
-        db.get(id).then(function (doc) {
+        db.get(id, {attachments: true, binary: true}).then(function (doc) {
           console.log("found");
           var commentSection = doc.notesAndComments + " \n---EDIT---\n ";
           var manipulatorType = doc.manipulatorType;
@@ -92,6 +92,26 @@ $(document).ready(function () {
                 $('#' + $(this).attr("id")).addClass('active');
             });
           }
+          var robotPhotoFrontUrl = URL.createObjectURL(doc._attachments['robotFront.jpg'].data);
+          var robotPhotoBackUrl = URL.createObjectURL(doc._attachments['robotBack.jpg'].data);
+          var robotPhotoLeftUrl = URL.createObjectURL(doc._attachments['robotLeft.jpg'].data);
+          var robotPhotoRightUrl = URL.createObjectURL(doc._attachments['robotRight.jpg'].data);
+          var robotPhotoTopUrl = URL.createObjectURL(doc._attachments['robotTop.jpg'].data);
+          var robotPhotoFrontPreview = document.getElementById("robotPhotoFrontPreview");
+          robotPhotoFrontPreview.src = robotPhotoFrontUrl;
+          var robotPhotoBackPreview = document.getElementById("robotPhotoBackPreview");
+          robotPhotoBackPreview.src = robotPhotoBackUrl;
+          var robotPhotoLeftPreview = document.getElementById("robotPhotoLeftPreview");
+          robotPhotoLeftPreview.src = robotPhotoLeftUrl;
+          var robotPhotoRightPreview = document.getElementById("robotPhotoRightPreview");
+          robotPhotoRightPreview.src = robotPhotoRightUrl;
+          var robotPhotoTopPreview = document.getElementById("robotPhotoTopPreview");
+          robotPhotoTopPreview.src = robotPhotoTopUrl;
+          document.getElementById("robotPhotoFrontPreview").style.display = "block";
+          document.getElementById("robotPhotoBackPreview").style.display = "block";
+          document.getElementById("robotPhotoLeftPreview").style.display = "block";
+          document.getElementById("robotPhotoRightPreview").style.display = "block";
+          document.getElementById("robotPhotoTopPreview").style.display = "block";  
         }).catch(function (err) {
             if (err.name === 'not_found') {
                 console.log("not found");
@@ -107,7 +127,22 @@ $(document).ready(function () {
                     $('#' + $(this).attr("id")).removeClass('active');
                 });
                 $('input[type=checkbox]').attr('checked',false);
-                $('input[type=radio]').attr('checked',false);      
+                $('input[type=radio]').attr('checked',false);
+                document.getElementById("robotPhotoFrontPreview").style.display = "none";
+                document.getElementById("robotPhotoBackPreview").style.display = "none";
+                document.getElementById("robotPhotoLeftPreview").style.display = "none";
+                document.getElementById("robotPhotoRightPreview").style.display = "none";
+                document.getElementById("robotPhotoTopPreview").style.display = "none";  
+                var robotPhotoFrontPreview = document.getElementById("robotPhotoFrontPreview");
+                robotPhotoFrontPreview.src = "#";
+                var robotPhotoBackPreview = document.getElementById("robotPhotoBackPreview");
+                robotPhotoBackPreview.src = "#";
+                var robotPhotoLeftPreview = document.getElementById("robotPhotoLeftPreview");
+                robotPhotoLeftPreview.src = "#";
+                var robotPhotoRightPreview = document.getElementById("robotPhotoRightPreview");
+                robotPhotoRightPreview.src = "#";
+                var robotPhotoTopPreview = document.getElementById("robotPhotoTopPreview");
+                robotPhotoTopPreview.src = "#";          
             } else {
                 console.log(err);
             }
